cb090992f03075900da3e1dcfdae1ab4637dacce
SatejSawant45 <satejsawant90@gmail.com>
2025-01-08 21:16:59 +0530

    Implemented App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import UserAuth from './pages/auth/UserAuth';
+import AdminAuth from './pages/auth/AdminAuth';
+import UserDashboard from './pages/user/Dashboard';
+import AdminDashboard from './pages/admin/Dashboard';
+import CreateQuiz from './pages/admin/CreateQuiz';
+import EditQuiz from './pages/admin/EditQuiz';
+import TakeQuiz from './pages/quiz/TakeQuiz';
+import VideoLectures from './pages/lectures/VideoLectures';
+import UserChat from './pages/chat/UserChat';
 
-import './App.css'
-import Home from './pages/Home.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import QuizCard from './componants/QuizCard.jsx';
-import AuthForm from './componants/AuthFrom.jsx';
-import LeaderboardTable from './componants/LeaderboardTable.jsx';
-import Navbar from './componants/Navbar.jsx';
-
-
-function App() 
+function App()
 {
-  
-
-  return (
-    <div>
-        <Navbar></Navbar>
-        <LeaderboardTable ></LeaderboardTable>
-    </div>
-  );
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout></Layout>}></Route>
+                <Route index element={<Home></Home>}></Route>
+                <Route path="/login" element={<UserAuth></UserAuth>}></Route>
+                <Route path="/signup" element={<UserAuth></UserAuth>}></Route>
+                <Route path="/admin/login" element={<AdminAuth></AdminAuth>}></Route>
+                <Route path="/admin/signup" element={<AdminAuth></AdminAuth>}></Route>
+                <Route path="dashboard" element={<UserDashboard></UserDashboard>}></Route>
+                <Route path="admin/dashboard" element={<AdminDashboard></AdminDashboard>}></Route>
+                <Route path="admin/quiz/create" element={<CreateQuiz></CreateQuiz>}></Route>
+                <Route path="admin/quiz/:id/edit" element={<EditQuiz></EditQuiz>}></Route>
+                <Route path="/quiz/:id" element={<TakeQuiz></TakeQuiz>}></Route>
+                
+            </Routes>
+        </Router>
+    )
 }
-
-export default App
---
+++ Frontend/src/componants/Layout.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+export default function Layout()
+{
+    return(
+        <div className="min-h-screen bg-gray-50">
+            <Navbar></Navbar>
+            <main className="container mx-auto px-4 py-8"></main>
+            <Outlet />
+
+        </div>
+    )
+}
